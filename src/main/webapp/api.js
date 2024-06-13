a656546ef842ca3deb3fe332e53b4c29ad616684
Ajor <ajor@MACs-MacBook-Pro.local>
2024-06-13 15:53:09 +0100

    feature: UI updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,6 @@
 //export const token = new URLSearchParams(window.location.search).get("jwt");
 //export const url = "/";
-export const url = "http://localhost:8383/";
+//export const url = "http://localhost:8383/api/v1/";
+export const url = "https://lamisplus.org/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE0NTE1Njk2fQ.qlhTyh3IczX5-L0OvQ9UA0ex9DfUJPODlVS907TnYrQZtCP2xOXkA3QDShRqg69y6D0ImQRlAlPSHeFlsyGzig";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4MzI2MjM1fQ.pRGFYDXP9jMtNzZlD_OP01cVUEJffPfKrWUPWm8ReRO27YE78YJbhuFVKlGVi2pTwV4l4bDjHkT9pyxj3ZH8CA";
---
+++ src/main/webapp/jsx/components/ADRForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ADRForm() {
+  return <div>ADRForm</div>;
+}
+
+export default ADRForm;
